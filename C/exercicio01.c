b8e8559fae59cbc613392eb338caa6ee4d87c399
Luis André Santos <luisandresantosdesign@gmail.com>
2025-06-14 23:07:03 +0100

    exercicio01 - linguagem C

---
+++ C/exercicio01.c
@@ -0,0 +1,14 @@
+/*
+ * Programa: Hello World em C
+ * Autor: Luis André Santos
+ * Objetivo: Primeiro programa para iniciar no GitHub.
+ * Descrição: Este programa imprime a mensagem "Hello, World!" no ecrã.
+ */
+
+#include <stdio.h>
+
+int main() {
+    // Imprime a mensagem no ecrã
+    printf("Hello, World!\n");
+    return 0;
+}
